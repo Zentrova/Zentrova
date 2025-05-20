69b047b41371fdd87ef7597f5ddac33cf0e60222
Yashpal Singh <yashpal1752@gmail.com>
2025-05-20 22:00:18 +0530

    hero section init

--- src/components/header.js
+++ src/components/header.js
@@ -27,30 +27,28 @@ export default function Header() {
     return (
         <header
             className={`sticky top-0 w-full z-50 transition-all duration-300 ${scrolled
-                    ? 'pt-3'
-                    : ''
+                ? 'pt-3'
+                : ''
                 }`}
         >
             <div className=
                 {` mx-auto flex justify-between items-center px-10 py-5 transition-all duration-300 ${scrolled
-                        ? 'backdrop-blur-sm bg-header sm shadow-md w-11/12 rounded-lg'
-                        : 'shadow-md w-full'
+                    ? 'backdrop-blur-sm bg-header sm shadow-md w-11/12 rounded-lg'
+                    : 'shadow-md w-full'
                     }`}>
-                {/* Logo */}
                 <Link href="/">
                     <p className="text-2xl font-bold tracking-wider">MyLogo</p>
                 </Link>
 
-                {/* Nav Links - Only visible on sm and above */}
                 <nav className="hidden sm:block">
                     <ul className="flex space-x-8 font-medium text-lg">
                         {navLinks.map((link) => {
                             const isActive = pathname === link.href;
                             return (
-                                <li key={link.href} className="group relative pb-1">
-                                    <Link href={link.href}>
+                                <Link href={link.href} key={link.href}>
+                                    <li className="group relative">
                                         <span
-                                            className={`transition-colors ${isActive ? 'text-primary font-semibold' : 'hover:text-primary'
+                                            className={`transition-colors ${isActive ? 'text-primary ' : 'group-hover:text-primary'
                                                 }`}
                                         >
                                             {link.label}
@@ -59,8 +57,8 @@ export default function Header() {
                                             className={`absolute left-0 -bottom-0.5 h-0.5 bg-primary transition-transform duration-300 ease-out origin-left ${isActive ? 'w-full scale-x-100' : 'w-full scale-x-0 group-hover:scale-x-100'
                                                 }`}
                                         />
-                                    </Link>
-                                </li>
+                                    </li>
+                                </Link>
                             );
                         })}
                     </ul>
