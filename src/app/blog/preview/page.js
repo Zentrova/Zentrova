9ebdb283b1ab9fd0ddb90bd215ca6e7e094eae30
Yashpal Singh <yashpal1752@gmail.com>
2025-06-16 22:05:59 +0530

    blog preview 50%

---
+++ src/app/blog/preview/page.js
@@ -0,0 +1,218 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function BlogPreview() {
+  return (
+    <div className='blogWrap mb-10'>
+      <section className='heroSection py-10'>
+        <p className='text-3xl font-bold text-center'>Blog Title</p>
+      </section>
+      <section className='flex px-20 gap-12'>
+        <div className=' w-1/4  flex flex-col px-2 '>
+          <div className='sticky top-32'>
+            <p className='text-xl font-semibold my-2'>Content</p>
+            <ul className='content-List'>
+              <li>POC vs Prototype vs MVP: Which One Do You Need?</li>
+              <li className='active'>POC vs Prototype vs MVP: Which One Do You Need?</li>
+              <li>POC vs Prototype vs MVP: Which One Do You Need?</li>
+              <li>POC vs Prototype vs MVP: Which One Do You Need?</li>
+              <li>POC vs Prototype vs MVP: Which One Do You Need?</li>
+              <li>POC vs Prototype vs MVP: Which One Do You Need?</li>
+              <li>POC vs Prototype vs MVP: Which One Do You Need?</li>
+              <li>POC vs Prototype vs MVP: Which One Do You Need?</li>
+            </ul>
+            <div className='w-full'>
+              <Image src='https://img.freepik.com/free-photo/technology-communication-icons-symbols-concept_53876-120314.jpg?semt=ais_hybrid&w=740' alt='Blog Thumnail' height={100} width={100} unoptimized  className='object-cover w-full'/>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-3/4'>
+          <p>In the fast-paced world of product development, concepts like POC, prototype, and MVP are frequently mentioned, but what do they actually signify? More crucially, which one should your startup or business prioritize first?
+            If you’re uncertain about where to staWhat Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.What Is a Proof of Concept (POC)?
+            A Proof of Concept (POC) is the earliest validation of an idea. It’s not about design or user interface. It’s about technical feasibility.
+            Think of it as:
+
+            “Will this idea even work before we invest more time and money?”
+
+            It’s typically used to test whether a specific technology, integration, or algorithm is even possible before you invest in full development.
+
+            Example: A healthtech startup wants to know if AI can detect early-stage lung cancer from CT scans. They build a proof of concept (POC) with anonymized data to test accuracy before developing the full product.
+
+            Best for:
+
+            Deep tech or AI products.
+            Startups testing innovation.
+            Internal tools exploring automation.
+            R&D projects.rt Proof of Concept, Prototype, or Minimum Viable Product, you’re not the only one. Each has a distinct function, and picking the wrong one can lead to wasted time, resources, and forward momentum.
+            Let’s clarify this with straightforward explanations, practical examples, and a decision-making framework that you can genuinely utilize.</p>
+        </div>
+      </section>
+    </div>
+  )
+}
